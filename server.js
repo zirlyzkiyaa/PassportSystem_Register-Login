4d2db27f395e332aa7019403f652b57f9afefdc2
Zirlyfera Zakiya Maulidia <99418260+zirlyzkiyaa@users.noreply.github.com>
2023-05-25 17:32:53 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,103 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+//library
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+var passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+const initializePassport = require('./passport-config');
+initializePassport(
+  passport, 
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = [];
+
+//mengatur mesin tampilan (view engine) yang akan digunakan dalam aplikasi.
+app.set("view engine", "ejs");
+
+//menggunakan framework express
+app.use(express.urlencoded({ extended: false }));
+
+//
+app.use(flash())
+
+//
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUnitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+//membuat server HTTP dan menentukan pengaturan rute
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+
+//login
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+//register
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+
+function checkAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(3000);
